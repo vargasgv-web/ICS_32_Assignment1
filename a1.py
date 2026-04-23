f3a432f57512b69f1f418f9e66593186a3793ff9
vargasgv-web <vargasgv@uci.edu>
2026-04-22 19:54:15 -0700

    Added the a1 file that will contain the code

---
+++ a1.py
@@ -0,0 +1,9 @@
+# a1.py
+
+# Starter code for assignment 1 in ICS 32 Programming with Software Libraries in Python
+
+# Replace the following placeholders with your information.
+
+# NAME
+# EMAIL
+# STUDENT ID
