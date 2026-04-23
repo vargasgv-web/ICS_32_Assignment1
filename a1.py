c9fc89bec08d27130ae1c56f31daa8cbe5f95050
vargasgv-web <vargasgv@uci.edu>
2026-04-22 21:38:11 -0700

    Update a1.py
    Added the Create (C) Command, still needs a lot of work.

--- a1.py
+++ a1.py
@@ -10,11 +10,48 @@
 
 from pathlib import Path
 
+
+
+def create_file(dir, file):
+    file = file
+    p = Path(dir) / file
+    p.touch()
+    print(p.resolve())
+
+def create_directory(dir):
+    p = Path(dir)
+    p.mkdir(parents=True)
+
+def directory_exists(dir):
+    p = Path(dir)
+    if p.exists():
+        return True
+    else:
+        return False
+
+def file_exists(dir, file):
+    p = Path(dir).joinpath(file)
+    if p.exists():
+        return True
+    else:
+        return False
+
 def run():
-    action = input("Please enter an action (enter C to create a new file, D to delete a file, "
+    action = input("Please enter an action (Enter C to create a new file, D to delete a file, "
                    "R to read a file, or Q to quit): ")
     if action == "C":
-        pass
+        dir = input("Please enter the directory name: ")
+        if not directory_exists(dir):
+            print("Error: Directory does not exist")
+        else:
+            file = input("Please enter the file name: ")
+            file = file + ".dsu"
+            if file_exists(dir, file):
+                print("Error: File already exists")
+            else:
+                create_file(dir, file)
+                print(file.resolve())
+                
     elif action == "D":
         pass
     elif action == "R":
