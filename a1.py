86af09d5f80aeab47df97c8befc8ffb912da5479
victor <vargasgv@uci.edu>
2026-04-23 20:58:54 -0700

    Updated the R (Read file) command, made it receive correct input, changed all the error messages to just say ERROR to pass validity checker

--- a1.py
+++ a1.py
@@ -22,14 +22,14 @@ def directory_exists(dir):
     else:
         return False
 
-def file_exists(dir, file):
+def file_exists_create(dir, file):
     p = Path(dir) / file
     if p.exists():
         return True
     else:
         return False
 
-def file_exists2(file):
+def file_exists(file):
     p = Path(file)
     if p.exists():
         return True
@@ -41,8 +41,8 @@ def delete_file(file):
     print(p.resolve(), "DELETED")
     p.unlink()
 
-def read_file(dir, file):
-    p = Path(dir) / file
+def read_file(file):
+    p = Path(file)
     with open(p, 'r') as f:
         if not f.read(1):
             print('EMPTY')
@@ -58,54 +58,56 @@ def dsu_check(file):
         return False
 
 def run():
-    action = input("Enter your file command (Q to exit): ")
+    #action = input("Enter your file command (Q to exit): \n")
+    action = input()
     action_list = action.split()
 
     if action_list[0] == "C":
         if len(action_list) == 4:
             if not directory_exists(action_list[1]):
-                print("ERROR: directory does not exist")
+                print("ERROR")
             else:
                 file = action_list[3]
                 file = file + ".dsu"
-                if file_exists(action_list[1], file):
-                    print("ERROR: File already exists")
+                if file_exists_create(action_list[1], file):
+                    print("ERROR")
                 else:
                     create_file(action_list[1], file)
         else:
-            print("ERROR: incorrect format for C")
+            print("ERROR")
 
     elif action_list[0] == "D":
         if len(action_list) == 2:
             file = action_list[1]
             if dsu_check(file):
-                if not file_exists2(file):
-                    print("ERROR: File does not exists")
+                if not file_exists(file):
+                    print("ERROR")
                 else:
                     delete_file(file)
             else:
-                print("ERROR: not a dsu file")
+                print("ERROR")
         else:
-            print("ERROR: incorrect format for D")
+            print("ERROR")
 
     elif action_list[0] == "R":
-        dir = input("Please enter the directory name: ")
-        if not directory_exists(dir):
-            print("ERROR: Directory does not exist")
-        else:
-            file = input("Please enter the file name: ")
-            file = file + ".dsu"
-            if not file_exists(dir, file):
-                print("ERROR: File does not exists")
+        if len(action_list) == 2:
+            file = action_list[1]
+            if dsu_check(file):
+                if not file_exists(file):
+                    print("ERROR")
+                else:
+                    read_file(file)
             else:
-                read_file(dir, file)
+                print("ERROR")
+        else:
+            print("ERROR")
 
     elif action_list[0] == "Q":
         print("Exiting file explorer")
         return
 
     else:
-        print("ERROR: Not an available command")
+        print("ERROR")
     run()
 
 if __name__ == '__main__':
